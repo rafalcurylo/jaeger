ceb4cf7b5345e9356e775ea622fb8939ad471e98
PLADBEM <PLADBEM@host.docker.internal>
2020-03-27 13:51:46 +0100

    SpringBoot rest aplications

---
+++ sb-rest-client/src/main/java/com/bemben/jaegerclient/restclient/util/Greeting.java
@@ -0,0 +1,15 @@
+package com.bemben.jaegerclient.restclient.util;
+
+public class Greeting {
+
+	private long id;
+	private String content;
+
+	public long getId() {
+		return id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+}
---
+++ sb-rest-client/src/main/java/com/bemben/jaegerclient/restclient/util/TracingUtil.java
@@ -0,0 +1,55 @@
+package com.bemben.jaegerclient.restclient.util;
+
+
+import io.jaegertracing.Configuration;
+import io.jaegertracing.Configuration.ReporterConfiguration;
+import io.jaegertracing.Configuration.SamplerConfiguration;
+import io.jaegertracing.Configuration.SenderConfiguration;
+import io.jaegertracing.internal.samplers.ConstSampler;
+import io.opentracing.Tracer;
+
+
+public class TracingUtil {
+
+    /*public static Tracer initTracer(String service) {
+        
+        SenderConfiguration senderConfiguration = new SenderConfiguration().withEndpoint("http://localhost:16686/api/traces");
+        
+        ReporterConfiguration reporterConfig = ReporterConfiguration.fromEnv().withLogSpans(true).withSender(senderConfiguration);
+        
+        io.opentracing.Tracer tracer = new Configuration(service).withReporter(reporterConfig)
+                // We need to get a builder so that we can directly inject the reporter instance.
+                .getTracerBuilder()
+                // This configures the tracer to send all spans, but you will probably want to use something less verbose.
+                .withSampler(new ConstSampler(true))
+                .build();
+        
+        return tracer;
+    }
+    */
+    
+	public static Tracer initTracer2(String service) {
+     
+        SamplerConfiguration samplerConfig = new SamplerConfiguration()
+        .withType(ConstSampler.TYPE)
+        .withParam(1);
+        
+        SenderConfiguration senderConfiguration = new SenderConfiguration()
+                    .withAgentHost("localhost")
+                    .withAgentPort(5778)
+                    .withEndpoint("http://localhost:14268/api/traces");
+                    //.withAuthToken(authToken)
+                    //.withAuthUsername(authUsername)
+                    //.withAuthPassword(authPassword);
+        
+        ReporterConfiguration reporterConfig = new ReporterConfiguration()
+        		.withFlushInterval(1000)
+                .withMaxQueueSize(1)
+                .withSender(senderConfiguration)
+                .withLogSpans(true);
+        
+        Configuration config = new Configuration(service).withSampler(samplerConfig).withReporter(reporterConfig);
+        return config.getTracer();
+    }
+    
+}
