97d9c10ba9afbdb10ce5130430c91193239b0c79
Tomasz Margosiak <tomasz.margosiak@pl.abb.com>
2020-04-03 11:35:56 +0200

    .NET->Java context

--- aspnet-app/Source/aspnet-app/Controllers/WeatherForecastController.cs
+++ aspnet-app/Source/aspnet-app/Controllers/WeatherForecastController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -24,8 +25,18 @@ namespace aspnet_app.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public async Task<IEnumerable<WeatherForecast>> Get()
         {
+
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("http://adam:9083/greeting?name=Test"))
+                //using (var response = await httpClient.GetAsync("http://localhost:9083/greeting?name=Test"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                }
+            }
+
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
